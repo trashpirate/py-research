d28756d21803ed692af244cdb7857ab6d97e66cf
trashpirate <nadinaz@sent.com>
2023-01-14 12:44:28 -0600

    add new setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_research",
+    version="0.0.1",
+    author="Nadina (Zweifel) Oates",
+    author_email="nadinaoates@gmail.com",
+    description="Research modules with functions for data import, plotting, signal processing, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trashpirate/py_research",
+    project_urls={"Bug Tracker": "https://github.com/trashpirate/py_research/issues"},
+    license="MIT",
+    packages=["py_research"],
+    install_requires=["numpy", "scipy", "scikit-learn", "matplotlib", "seaborn", "cycler"],
+)
