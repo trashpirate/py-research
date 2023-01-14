359b1a059e2a3071be90b265f0d51fc498c05bda
trashpirate <nadinaz@sent.com>
2023-01-14 14:23:48 -0600

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py_research",
+    name="pyresearch",
     version="0.0.1",
     author="Nadina (Zweifel) Oates",
     author_email="nadinaoates@gmail.com",
